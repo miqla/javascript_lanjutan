f7ea7c5b524779f3fb13e253982b0d44222ee8f9
miqla <miqdad.shaldin@gmail.com>
2023-11-12 16:55:42 +0700

    menambahkan materi tentang rest parameter, contoh penggunaan pada array destructuring, objek destructuring, dan filtering

--- spread.js
+++ spread.js
@@ -34,3 +34,45 @@ const ab = [...judul.textContent]
   .join("");
 console.log(ab);
 judul.innerHTML = ab;
+
+// Rest parameter --------------------- // ------------------------------------//
+function myFunc(a, b, ...rest) {
+  // return a;
+  // return `a = ${a}, b = ${b}, sisa = ${rest}`;
+  // return rest;
+  // return arguments;
+  return Array.from(arguments);
+}
+console.log(myFunc(1, 2, 3, 4, 5));
+
+function jumlahkan(...angka) {
+  // let total = 0;
+  // for (const a of angka) {
+  //   total += a;
+  // }
+  // return total;
+
+  return angka.reduce((a, b) => a + b);
+}
+console.log(jumlahkan(1, 2, 3, 4, 5));
+
+// array destructuring
+const kelompok1 = ["ada", "nasi", "kuning", "dan", "pecel"];
+const [ketua, wakil, ...anggota] = kelompok1;
+console.log(anggota);
+
+// objek destructuring
+const kelompok2 = {
+  pm: "badak",
+  backend1: "ayam",
+  backend12: "gla",
+  normal: "bela",
+};
+const { pm, ...sisa } = kelompok2;
+console.log(sisa);
+
+// filtering
+function filterBy(type, ...value) {
+  return value.filter((v) => typeof v === type);
+}
+console.log(filterBy("string", 1, 2, "bima", "hilih", true, false));
