0876897e0c8a1003b0fe3582bce0b5af291890a6
miqla <miqdad.shaldin@gmail.com>
2023-12-12 22:31:02 +0700

    modif objek.html manggil file baru callback.js, isinya contoh callback, lalu contoh higher order function callback synchronous

---
+++ callback.js
@@ -0,0 +1,37 @@
+// Callback
+// Synchronous callback: sebuah function yg parameternya juga function
+
+// function halo(nama) {
+//   alert(`Halo ${nama}`);
+// }
+
+// function tampilkanNama(callback) {
+//   const nama = prompt("Masukkan nama!");
+//   callback(nama);
+// }
+// // tampilkanNama(halo);
+// tampilkanNama((nama) => alert(`Halo ${nama}`));
+
+const mhs = [
+  {
+    nama: "alek",
+    umur: "40",
+  },
+  {
+    nama: "mila",
+    umur: "20",
+  },
+  {
+    nama: "beliau",
+    umur: "doi",
+  },
+];
+console.log("mulai");
+mhs.forEach((m) => {
+  // kasih for biar loopingnya lama, buat contoh minus synchronous
+  for (let i = 0; i < 10000000; i++) {
+    const date = new Date();
+  }
+  console.log(m.nama);
+});
+console.log("selesai");
