839ed93f58bfceeaac02afb0af90510c9414d76c
miqla <miqdad.shaldin@gmail.com>
2023-12-19 22:10:33 +0700

    membuat file omdb.html dan . js, membangung HTML dengan bootstrap, JS menggunakan ajax untuk memanggil open API dari IMDB dan membuat card untuk 10 judul film

---
+++ omdb.js
@@ -0,0 +1,24 @@
+$.ajax({
+  url: "http://www.omdbapi.com/?apikey=38c2cf77&s=one piece",
+  success: (result) => {
+    // biar gadda search nya langsung masuk ke array
+    const movies = result.Search;
+    console.log(movies);
+    let cards = "";
+    movies.forEach((m) => {
+      cards += `<div class="col-md-4 my-3">
+                  <div class="card">
+                    <img src="${m.Poster}" class="card-img-top" />
+                    <div class="card-body">
+                      <h5 class="card-title">${m.Title}</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                      <a href="#" class="btn btn-primary">Show details</a>
+                    </div>
+                  </div>
+                </div>`;
+      // JQuery carikan elemen dengan class..., lalu html nya sisipkan cards
+      $(".movie-container").html(cards);
+    });
+  },
+  error: (e) => console.log(e.responseText),
+});
