67e8c28c5f343580391624f6b2193cb485e3f29f
miqla <miqdad.shaldin@gmail.com>
2023-12-21 21:34:55 +0700

    membuat tombol detail mengirimkan info ketika di klik, menambahkan informasi detail untuk ditampilkan di modal box, merapihkan struktur HTML didalam omdb.js kedalam function

--- omdb.js
+++ omdb.js
@@ -1,25 +1,71 @@
 $.ajax({
-  url: "http://www.omdbapi.com/?apikey=38c2cf77&s=furious",
+  url: "http://www.omdbapi.com/?apikey=38c2cf77&s=naruto",
   success: (result) => {
     // biar gadda search nya langsung masuk ke array
     const movies = result.Search;
     console.log(movies);
     let cards = "";
     movies.forEach((m) => {
-      cards += `<div class="col-md-4 my-3">
-                  <div class="card">
-                    <img src="${m.Poster}" class="card-img-top" />
-                    <div class="card-body">
-                      <h5 class="card-title">${m.Title}</h5>
-                      <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-                      <a href="#" class="btn btn-primary" data-bs-toggle="modal"
-                      data-bs-target="#movieDetailModal">Show details</a>
-                    </div>
-                  </div>
-                </div>`;
+      cards += showCard(m);
       // JQuery carikan elemen dengan class..., lalu html nya sisipkan cards
       $(".movie-container").html(cards);
+
+      // Ketika tombol diklik
+      $(".tombol-detail").on("click", function () {
+        $.ajax({
+          url:
+            "http://www.omdbapi.com/?apikey=38c2cf77&i=" +
+            $(this).data("imdbid"),
+          success: (m) => {
+            const movieDetail = showMovieDetail(m);
+            $(".modal-body").html(movieDetail);
+            console.log(m);
+          },
+          error: (e) => console.log(e.responseText),
+        });
+      });
     });
   },
   error: (e) => console.log(e.responseText),
 });
+
+function showCard(m) {
+  return `<div class="col-md-4 my-3">
+  <div class="card">
+    <img src="${m.Poster}" class="card-img-top" />
+    <div class="card-body">
+      <h5 class="card-title">${m.Title}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+      <a href="#" class="btn btn-primary tombol-detail" data-bs-toggle="modal"
+      data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show details</a>
+    </div>
+  </div>
+</div>`;
+}
+
+function showMovieDetail(m) {
+  return `<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-3">
+      <img src="${m.Poster}" class="img-fluid" />
+    </div>
+    <div class="col-md">
+      <ul class="list-group">
+        <li class="list-group-item"><h4>${m.Title} ${m.Year}</h4></li>
+        <li class="list-group-item">
+          <strong>Director : </strong> ${m.Director}
+        </li>
+        <li class="list-group-item">
+          <strong>Actors: </strong> ${m.Actors}
+        </li>
+        <li class="list-group-item">
+          <strong>Writer : </strong>${m.Writer}
+        </li>
+        <li class="list-group-item">
+          <strong>Plot: </strong>${m.Plot}
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>`;
+}
